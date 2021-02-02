b73a7fc97e4c3b284a56923bac54f4e0ee4e7e7b
Ssmartcode <deico@DESKTOP-P9EHPBC.localdomain>
2021-02-02 19:37:15 +0200

    version 2.0

--- test.js
+++ test.js
@@ -1,39 +1,73 @@
-'use strict';
+"use strict";
 
-const main = document.querySelector('main');
-const network = document.querySelector('.network');
-const networkIcon = document.querySelector('.fa-network-wired');
-const homeIcon = document.querySelector('.fa-home');
-const content = document.querySelector('.content');
+const home = document.querySelector("#home");
+const about = document.querySelector(".about");
+const contact = document.querySelector("#contact");
+const aboutIcon = document.querySelector(".about-icon");
+const homeIcon = document.querySelector(".home-icon");
+const contactIcon = document.querySelector(".contact-icon");
+const contactMeLink = document.querySelector(".contact-link");
 
-let currentPage = main;
+const footer = document.querySelector("footer");
+const footerCross = document.querySelector(".cross");
+
+let typeEffectParagraph = document.querySelector(".type-effect");
+
+const linkIcons = [aboutIcon, homeIcon, contactIcon];
+let currentPage = home;
 
 // SCROLL TO TOP FUNCTION
 const scrollToTop = () => {
   window.scroll({
     top: 0,
-    behavior: 'smooth',
+    behavior: "smooth",
   });
 };
-// SWITCH PAGE FUNCTION
-const switchPage = (current, next) => {
-  current.classList.add('hidden');
-  next.classList.remove('hidden');
-  currentPage = next;
-  scrollToTop();
+// HIDE THE FOOTER
+footerCross.addEventListener("click", () => (footer.style.display = "none"));
+// ADD SECONDARY COLOR TO THE ICON WHICH IS RELATED TO THE ACTIVE PAGE
+const toggleActivePage = function () {
+  linkIcons.forEach((icon) => icon.classList.remove("active-page"));
+  this.classList.toggle("active-page");
 };
-function checkHeight(element) {
-  console.log(element.getBoundingClientRect());
-}
+
+// SWITCH PAGE FUNCTION - HIDES CURRENT PAGE AND SHOWS THE NEXT ONE
+const switchPage = (current, next) => {
+  current.classList.toggle("hidden");
+  next.classList.toggle("hidden");
+  currentPage = next;
+  // scrollToTop();
+};
+
 // EVENT LISTENERS FOR LINK ICONS
-networkIcon.addEventListener('click', function () {
-  switchPage(currentPage, network);
-  console.log(network.clientHeight);
-  network.parentElement.style.overflowY = 'hidden';
+aboutIcon.addEventListener("click", function () {
+  switchPage(currentPage, about);
+  toggleActivePage.call(aboutIcon);
 });
-homeIcon.addEventListener('click', function () {
-  switchPage(currentPage, main);
-  network.parentElement.style.overflowY = 'auto';
+homeIcon.addEventListener("click", function () {
+  switchPage(currentPage, home);
+  toggleActivePage.call(homeIcon);
 });
-console.log(content.clientHeight, content.parentElement.scrollHeight);
-console.log(network.scrollHeight, network.offsetHeight, network.clientHeight);
+contactIcon.addEventListener("click", function () {
+  switchPage(currentPage, contact);
+  toggleActivePage.call(contactIcon);
+});
+
+// EVENT LISTENER FOR CONTACT ME LINK ON TOP OF THE PAGE
+contactMeLink.addEventListener("click", () => {
+  switchPage(currentPage, contact);
+  toggleActivePage.call(contactIcon);
+});
+
+// TYPING EFFECT
+(function type(
+  index = 0,
+  text = "Hello world. I am Andrew. Wordpress developer. "
+) {
+  typeEffectParagraph.textContent += text[index++];
+  if (index == text.length) {
+    index = 0;
+    typeEffectParagraph.textContent = "";
+  }
+  setTimeout(() => type(index), 200);
+})();
